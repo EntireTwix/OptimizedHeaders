7e6bfb3b11525191d2a99898bb4a6c06474d4d83
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-10 19:22:25 -0700

    :construction: in progress

--- fast_mat.h
+++ fast_mat.h
@@ -4,7 +4,7 @@ template <size_t W, size_t H, typename T>
 class FastMat
 {
 private:
-    T members[W * H];
+    T members[H][W];
 
 public:
     FastMat() = default;
@@ -15,4 +15,10 @@ public:
     constexpr size_t Area() const { return W * H; }
     constexpr size_t Width() const { return W; }
     constexpr size_t Height() const { return H; }
+
+    T &At(size_t x, size_t y) { return members[y][x]; }
+    T At(size_t x, size_t y) const { return members[y][x]; }
+
+    T *begin() { return &members[0][0]; }
+    T *end() { return &members[H][W]; }
 };
