b387fdeb22982f5963f1b804daf9f87b3371e247
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-10 20:58:59 -0700

    :racehorse: made all constexpr for performance

--- fast_mat.h
+++ fast_mat.h
@@ -8,31 +8,31 @@ private:
     T members[H][W];
 
 public:
-    FastMat() = default;
-    FastMat(auto &&... args) : members{args...} { static_assert(sizeof...(args) == W * H, "initilizing FastMat with an initilizer list much match the Area in size"); }
+    constexpr FastMat() = default;
+    constexpr FastMat(auto &&... args) : members{args...} { static_assert(sizeof...(args) == W * H, "initilizing FastMat with an initilizer list much match the Area in size"); }
 
     constexpr size_t Area() const { return W * H; }
     constexpr size_t Width() const { return W; }
     constexpr size_t Height() const { return H; }
 
-    T &At(size_t x, size_t y);
-    T At(size_t x, size_t y) const;
-    T *AtP(size_t x, size_t y);
+    constexpr T &At(size_t x, size_t y);
+    constexpr T At(size_t x, size_t y) const;
+    constexpr T *AtP(size_t x, size_t y);
 
-    T *begin() { return &members[0][0]; }
-    T *end() { return &members[H][W]; }
+    constexpr T *begin() { return &members[0][0]; }
+    constexpr T *end() { return &members[H][W]; }
 };
 
 template <size_t W, size_t H, typename T>
-inline T &FastMat<W, H, T>::At(size_t x, size_t y)
+constexpr inline T &FastMat<W, H, T>::At(size_t x, size_t y)
 {
     if ((x >= W) || (y >= H))
-        throw std::out_of_range("&At: out of range");
+        throw std::out_of_range("At: out of range");
     return members[y][x];
 }
 
 template <size_t W, size_t H, typename T>
-inline T FastMat<W, H, T>::At(size_t x, size_t y) const
+constexpr inline T FastMat<W, H, T>::At(size_t x, size_t y) const
 {
     if ((x >= W) || (y >= H))
         throw std::out_of_range(" At: out of range");
@@ -40,7 +40,7 @@ inline T FastMat<W, H, T>::At(size_t x, size_t y) const
 }
 
 template <size_t W, size_t H, typename T>
-inline T *FastMat<W, H, T>::AtP(size_t x, size_t y)
+constexpr inline T *FastMat<W, H, T>::AtP(size_t x, size_t y)
 {
     if ((x >= W) || (y >= H))
         throw std::out_of_range("*At: out of range");
