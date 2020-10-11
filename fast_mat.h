073a8ea86a7d238c3a55a16ca57048ee56c3a8f8
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-10 22:47:15 -0700

    :construction: :racehorse:  Transform and ApplyFunction functions

--- fast_mat.h
+++ fast_mat.h
@@ -1,4 +1,6 @@
 #include <stdexcept>
+#include <functional>
+#include <memory>
 
 template <size_t W, size_t H, typename T>
 class FastMat
@@ -14,34 +16,130 @@ public:
     constexpr size_t Width() const { return W; }
     constexpr size_t Height() const { return H; }
 
-    T &At(size_t x, size_t y);
-    constexpr T At(size_t x, size_t y) const;
-    T *AtP(size_t x, size_t y);
-
     T *begin() { return &members[0][0]; }
     T *end() { return &members[H][W]; }
+
+    //At functions
+    T &At(size_t x, size_t y)
+    {
+        if ((x >= W) || (y >= H))
+            throw std::out_of_range("&At: out of range");
+        return members[y][x];
+    }
+    constexpr T At(size_t x, size_t y) const
+    {
+        if ((x >= W) || (y >= H))
+            throw std::out_of_range(" At: out of range");
+        return members[y][x];
+    }
+    T *AtP(size_t x, size_t y)
+    {
+        if ((x >= W) || (y >= H))
+            throw std::out_of_range("*At: out of range");
+        return &members[y][x];
+    }
+
+    //Transform functions
+    FastMat Transform(const std::function<T(const T &)> &Func) const
+    {
+        FastMat res;
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                res.members[i][j] = Func(this->At(j, i));
+            }
+        }
+        return res;
+    }
+    template <typename ConstexprFunc>
+    constexpr FastMat Transform(const ConstexprFunc &Func, bool CORD_PARAMS = false) const
+    {
+        FastMat res;
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                if (CORD_PARAMS)
+                    res.members[i][j] = Func(this->At(j, i), j, i);
+                else
+                    res.members[i][j] = Func(this->At(j, i));
+            }
+        }
+        return res;
+    }
+    FastMat Transform(std::function<T(const T &, const T &)> Func, const FastMat &mat) const
+    {
+        FastMat res;
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                res.members[i][j] = Func(this->At(j, i), mat.At(j, i));
+            }
+        }
+        return res;
+    }
+    template <typename ConstexprFunc>
+    constexpr FastMat Transform(const ConstexprFunc &Func, const FastMat &mat, bool CORD_PARAMS = false) const
+    {
+        FastMat res;
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                if (CORD_PARAMS)
+                    res.members[i][j] = Func(this->At(j, i), j, i);
+                else
+                    res.members[i][j] = Func(this->At(j, i));
+            }
+        }
+        return res;
+    }
+    FastMat Transform(const std::function<T(const T &, size_t, size_t)> &Func) const
+    {
+        FastMat res;
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                res.members[i][j] = Func(this->At(j, i), j, i);
+            }
+        }
+        return res;
+    }
+    FastMat Transform(std::function<T(const T &, const T &, size_t, size_t)> Func, const FastMat &mat) const
+    {
+        FastMat res;
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                res.members[i][j] = Func(this->At(j, i), mat.At(j, i), j, i);
+            }
+        }
+        return res;
+    }
+
+    //ApplyFunction function
+    void ApplyFunction(const std::function<void(T &)> &Func)
+    {
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                this->At(i, j) = Func(this->At(j, i));
+            }
+        }
+    }
+    void ApplyFunction(const std::function<void(T &, size_t, size_t)> &Func)
+    {
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                this->At(i, j) = Func(this->At(j, i), j, i);
+            }
+        }
+    }
 };
-
-template <size_t W, size_t H, typename T>
-inline T &FastMat<W, H, T>::At(size_t x, size_t y)
-{
-    if ((x >= W) || (y >= H))
-        throw std::out_of_range("At: out of range");
-    return members[y][x];
-}
-
-template <size_t W, size_t H, typename T>
-constexpr inline T FastMat<W, H, T>::At(size_t x, size_t y) const
-{
-    if ((x >= W) || (y >= H))
-        throw std::out_of_range(" At: out of range");
-    return members[y][x];
-}
-
-template <size_t W, size_t H, typename T>
-inline T *FastMat<W, H, T>::AtP(size_t x, size_t y)
-{
-    if ((x >= W) || (y >= H))
-        throw std::out_of_range("*At: out of range");
-    return members[y][x];
-}
