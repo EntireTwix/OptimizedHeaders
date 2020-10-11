0c5d211393e4a2bf7c63fb6f51fdfe44ce3eadfa
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-10 20:45:05 -0700

    :construction: :bug: fixed At funcs

--- fast_mat.h
+++ fast_mat.h
@@ -15,25 +15,34 @@ public:
     constexpr size_t Width() const { return W; }
     constexpr size_t Height() const { return H; }
 
-    T &At(size_t x, size_t y)
-    {
-        if ((x >= W) || (y >= H))
-            throw std::out_of_range("&At: out of range");
-        return members[y][x];
-    }
-    T At(size_t x, size_t y) const
-    {
-        if ((x >= W) || (y >= H))
-            throw std::out_of_range(" At: out of range");
-        return members[y][x];
-    }
-    T *AtP(size_t x, size_t y)
-    {
-        if ((x >= W) || (y >= H))
-            throw std::out_of_range("*At: out of range");
-        return members[y][x];
-    }
+    T &At(size_t x, size_t y);
+    T At(size_t x, size_t y) const;
+    T *AtP(size_t x, size_t y);
 
     T *begin() { return &members[0][0]; }
     T *end() { return &members[H][W]; }
 };
+
+template <size_t W, size_t H, typename T>
+inline T &FastMat<W, H, T>::At(size_t x, size_t y)
+{
+    if ((x >= W) || (y >= H))
+        throw std::out_of_range("&At: out of range");
+    return members[y][x];
+}
+
+template <size_t W, size_t H, typename T>
+inline T FastMat<W, H, T>::At(size_t x, size_t y) const
+{
+    if ((x >= W) || (y >= H))
+        throw std::out_of_range(" At: out of range");
+    return members[y][x];
+}
+
+template <size_t W, size_t H, typename T>
+inline T *FastMat<W, H, T>::AtP(size_t x, size_t y)
+{
+    if ((x >= W) || (y >= H))
+        throw std::out_of_range("*At: out of range");
+    return members[y][x];
+}
