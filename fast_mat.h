72ba60b08049f95da2bc7b3fe6fa29cd2e650ca7
William Katz <qualitycontentquestionmark@gmail.com>
2020-10-10 20:34:53 -0700

    :construction:

--- fast_mat.h
+++ fast_mat.h
@@ -8,16 +8,27 @@ private:
 
 public:
     FastMat() = default;
-
-    template <typename... Params>
-    FastMat(Params &&... args) : members{args...} {}
+    FastMat(auto &&... args) : members{args...} { static_assert(sizeof...(args) == W * H, "initilizing FastMat with an initilizer list much match the Area in size"); }
 
     constexpr size_t Area() const { return W * H; }
     constexpr size_t Width() const { return W; }
     constexpr size_t Height() const { return H; }
 
-    T &At(size_t x, size_t y) { return members[y][x]; }
-    T At(size_t x, size_t y) const { return members[y][x]; }
+    T &At(size_t x, size_t y)
+    {
+        static_assert((x >= W) || (y >= H), "&At: out of range");
+        return members[y][x];
+    }
+    T At(size_t x, size_t y) const
+    {
+        static_assert((x >= W) || (y >= H), "At: out of range");
+        return members[y][x];
+    }
+    T *AtP(size_t x, size_t y)
+    {
+        static_assert((x >= W) || (y >= H), "*AtP: out of range");
+        return &members[y][x];
+    }
 
     T *begin() { return &members[0][0]; }
     T *end() { return &members[H][W]; }
