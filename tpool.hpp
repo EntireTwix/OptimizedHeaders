e06833bfe28866b0e55545fc8037231434f1284e
William Katz <qualitycontentquestionmark@gmail.com>
2020-11-17 10:50:29 -0800

    made Jobs const member

--- tpool.hpp
+++ tpool.hpp
@@ -3,7 +3,7 @@
 #include <mutex>
 #include <thread>
 #include <condition_variable>
-#include <queue>       
+#include <queue>
 #include <functional>
 
 template <uint_fast8_t threads = 0>
@@ -12,10 +12,10 @@ class ThreadPool final
 private:
     bool stopped = false, paused = true;
 
-    std::mutex* threadLocks; //lock for using jobs que
-    std::condition_variable* jobListener; //listener for waiting for jobs update
-    std::queue<std::function<void()> >* jobs; //a queue for each thread
-    std::thread* workers; //threads
+    std::mutex *threadLocks;                 //lock for using jobs que
+    std::condition_variable *jobListener;    //listener for waiting for jobs update
+    std::queue<std::function<void()>> *jobs; //a queue for each thread
+    std::thread *workers;                    //threads
     uint_fast8_t threadCount;
 
     void Stop()
@@ -26,50 +26,49 @@ private:
             jobListener[i].notify_one();
         }
     }
+
 public:
     ThreadPool()
     {
-        threadCount = threads?threads : std::thread::hardware_concurrency();
+        threadCount = threads ? threads : std::thread::hardware_concurrency();
         threadLocks = new std::mutex[threadCount];
         jobListener = new std::condition_variable[threadCount];
-        jobs = new std::queue<std::function<void()> >[threadCount];
+        jobs = new std::queue<std::function<void()>>[threadCount];
         workers = new std::thread[threadCount];
 
         for (uint_fast8_t i = 0; i < threadCount; ++i)
             workers[i] = std::thread([this, i]() {
-            std::function<void()> job;
+                std::function<void()> job;
 
-            while (!stopped)
-            {
-                //grab ownership
-                std::unique_lock<std::mutex> jobsAccess{ threadLocks[i] };
-
-                //wait for new jobs
-                if (jobs[i].empty() && !stopped)
+                while (!stopped)
                 {
-                    jobListener[i].wait(jobsAccess, [this, i]() { return !jobs[i].empty() || stopped; });
+                    //grab ownership
+                    std::unique_lock<std::mutex> jobsAccess{threadLocks[i]};
+
+                    //wait for new jobs
+                    if (jobs[i].empty() && !stopped)
+                    {
+                        jobListener[i].wait(jobsAccess, [this, i]() { return !jobs[i].empty() || stopped; });
+                    }
+
+                    //set up new job
+                    job = jobs[i].front();
+
+                    //wait while paused
+                    while (paused && !stopped)
+                    {
+                    }
+
+                    if (!paused && job)
+                    {
+                        //do work
+                        job();
+                        //pop job because its done
+                        jobs[i].pop();
+                    }
                 }
-
-                //set up new job
-                job = jobs[i].front();
-
-                //wait while paused
-                while (paused && !stopped)
-                {
-                }
-
-                if (!paused && job)
-                {
-                    //do work
-                    job();
-                    //pop job because its done
-                    jobs[i].pop();
-                }
-
-            }
-                });
+            });
     }
-
 
     void AddTask(std::function<void()> func)
     {
@@ -78,8 +77,8 @@ public:
         uint_fast8_t index;
         for (uint_fast8_t i = 0; i < threadCount; ++i)
         {
-            std::unique_lock<std::mutex> jobsAccess{ threadLocks[index] };
-            if (jobs[i].size()<smallest)
+            std::unique_lock<std::mutex> jobsAccess{threadLocks[index]};
+            if (jobs[i].size() < smallest)
             {
                 smallest = jobs[i].size();
                 index = i;
@@ -88,17 +87,17 @@ public:
 
         //adding to its job queue
         {
-            std::unique_lock<std::mutex> jobsAccess{ threadLocks[index] };
+            std::unique_lock<std::mutex> jobsAccess{threadLocks[index]};
             jobs[index].push(func);
         }
         jobListener[index].notify_all();
     }
-    size_t Jobs()
+    size_t Jobs() const
     {
         size_t sum = 0;
         for (uint_fast8_t i = 0; i < threadCount; ++i)
         {
-            std::unique_lock<std::mutex> jobsAccess{ threadLocks[i] };
+            std::unique_lock<std::mutex> jobsAccess{threadLocks[i]};
             sum += jobs[i].size();
         }
         return sum;
@@ -116,9 +115,12 @@ public:
     ~ThreadPool()
     {
         Stop();
-        //closing threads properly 
+        //closing threads properly
         for (size_t i = 0; i < threadCount; ++i)
-            if (workers[i].joinable()) workers[i].join();
+        {
+            if (workers[i].joinable())
+                workers[i].join();
+        }
         delete[] threadLocks;
         delete[] jobListener;
         delete[] jobs;
