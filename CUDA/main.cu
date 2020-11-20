a52b89be02968e405a5ad5d90da675f881ad536e
William Katz <qualitycontentquestionmark@gmail.com>
2020-11-20 10:42:38 -0800

    added multi threading for performance

--- CUDA/main.cu
+++ CUDA/main.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tpool.hpp>
 #include <CUDA/mat.hpp>
 
 #include <ratio>
@@ -8,47 +9,47 @@ int main()
 {
     double g_res, c_res;
     int sum;
-    for (int j = 0; j < 10; ++j)
+    ThreadPool pool;
+    pool.Start();
+
+    for (int j = 0; j < 1000; ++j)
     {
-        for (int i = 1;; i += 10)
-        {
-            system("CLS");
-            fMat a(1, i);
-            fMat b(i, 1);
 
-            std::cout << "elements " << i * 2 << '\n';
-
-            using namespace std::chrono;
-
-            std::cout << "With GPU\n";
+        pool.AddTask([&g_res, &c_res, &sum]() {
+            for (int i = 1;; i += 10)
             {
-                high_resolution_clock::time_point t1 = high_resolution_clock::now();
-                a.DotBench(b, true);
-                high_resolution_clock::time_point t2 = high_resolution_clock::now();
-                duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-                std::cout << "It took me " << time_span.count() << " seconds.";
-                std::cout << std::endl;
-                g_res = time_span.count();
-            }
+                fMat a(1, i);
+                fMat b(i, 1);
 
-            std::cout << "With CPU\n";
-            {
-                high_resolution_clock::time_point t1 = high_resolution_clock::now();
-                a.DotBench(b, false);
-                high_resolution_clock::time_point t2 = high_resolution_clock::now();
-                duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-                std::cout << "It took me " << time_span.count() << " seconds.";
-                std::cout << std::endl;
-                c_res = time_span.count();
+                using namespace std::chrono;
+
+                {
+                    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+                    a.DotBench(b, true);
+                    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+                    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+                    g_res = time_span.count();
+                }
+
+                {
+                    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+                    a.DotBench(b, false);
+                    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+                    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+
+                    c_res = time_span.count();
+                }
+                if (g_res < c_res)
+                {
+                    sum += i;
+                    break;
+                }
             }
-            if (g_res < c_res)
-            {
-                sum += i;
-                break;
-            }
-        }
+        });
     }
+    while (pool.Jobs())
+        ;
 
-    std::cout << sum / 5 << '\n';
+    std::cout << sum / 500 << '\n';
     return 0;
 }
