8c5db6b733c66afd8f153c2270ac77625db00dc8
William Katz <willkatz.dev@gmail.com>
2021-02-18 22:38:24 -0800

    :fire: removed unnecessary funcs

--- fast_mat.hpp
+++ fast_mat.hpp
@@ -36,44 +36,4 @@ public:
             throw std::out_of_range("*At: out of range");
         return &members[y][x];
     }
-
-    template <typename Function>
-    constexpr Mat Transform(Function&& Func) const
-    {
-        Mat res(sizeX, sizeY);
-        for (SizeT i = 0; i < sizeY; ++i)
-        {
-            for (SizeT j = 0; j < sizeX; ++j)
-            {
-                res.At(j, i) = Func(this->At(j, i), j, i);
-            }
-        }
-        return res;
-    }
-
-    template <typename Function>
-    constexpr void ApplyFunction(Function&& Func)
-    {
-        for (SizeT i = 0; i < sizeY; ++i)
-        {
-            for (SizeT j = 0; j < sizeX; ++j)
-            {
-                Func(this->At(j, i), j, i);
-            }
-        }
-    }
-
-    
-    friend std::ostream &operator<<(std::ostream &os, const FastMat &mat)
-    {
-        for (size_t i = 0; i < mat.SizeY(); ++i)
-        {
-            for (size_t j = 0; j < mat.SizeX(); ++j)
-            {
-                os << mat.At(j, i) << ' ';
-            }
-            os << '\n';
-        }
-        return os;
-    }
 };
