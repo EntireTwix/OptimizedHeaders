a159c93eaa8e2fee96bde8ae0f686c2e7bd6eebf
William Katz <willkatz.dev@gmail.com>
2021-03-08 13:29:53 -0800

    :sparkles: file i/o helper functions

--- utils.hpp
+++ utils.hpp
@@ -1,4 +1,8 @@
 #pragma once
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
 #include <type_traits>
 
 //consteval for non-C++20
@@ -13,9 +17,70 @@ using copy_fast_t = typename copy_fast<T>::type;
 
 //CombineBits
 template <typename T>
-unsigned char* CombineBits(T val) { return val; }
+inline unsigned char* CombineBits(T val) { return val; }
 template <typename T, typename... Bits>
-unsigned char* CombineBits(T head, Bits... vars) { return (head << sizeof(T)) + CombineBits(vars...); }
+inline unsigned char* CombineBits(T head, Bits... vars) { return (head << sizeof(T)) + CombineBits(vars...); }
+
+inline std::vector<std::string> ReadFileSimple(const std::string &file, char delim)
+{
+    std::ifstream temp(file);
+    std::vector<std::string> res;
+    std::string line;
+    if (temp.is_open())
+    {
+        while (std::getline(temp, line, delim))
+        {
+            res.push_back(line);
+        }
+        temp.close();
+    }
+    return res;
+}
+
+//File I/O
+template <typename T>
+inline std::vector<std::string> ReadFileSimple(const std::string &file, char delim, const T &func)
+{
+    std::ifstream temp(file);
+    std::vector<std::string> res;
+    std::string line;
+    if (temp.is_open())
+    {
+        while (std::getline(temp, line, delim))
+        {
+            func(line);
+            res.push_back(line);
+        }
+        temp.close();
+    }
+    return res;
+}
+
+inline std::vector<std::string> Split(const std::string &inp, char delim)
+{
+    std::stringstream ss(inp);
+    std::vector<std::string> res;
+    std::string line;
+    while (std::getline(ss, line, delim))
+    {
+        res.push_back(line);
+    }
+    return res;
+}
+
+template <typename T>
+inline std::vector<std::string> Split(const std::string &inp, char delim, const T &func)
+{
+    std::stringstream ss(inp);
+    std::vector<std::string> res;
+    std::string line;
+    while (std::getline(ss, line, delim))
+    {
+        func(line);
+        res.push_back(line);
+    }
+    return res;
+}
 
 /* nlohmann/json I/O 
 
