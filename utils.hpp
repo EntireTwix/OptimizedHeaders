069237cddc399486dc217d0bcf9e30f37b76cfda
William Katz <willkatz.dev@gmail.com>
2021-03-13 19:50:14 -0800

    removed CombineBits

--- utils.hpp
+++ utils.hpp
@@ -10,9 +10,3 @@ template <typename T>
 struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &> {};
 template <typename T>
 using copy_fast_t = typename copy_fast<T>::type;
-
-//CombineBits
-template <typename T, typename T2>
-constexpr auto CombineBits(T a, T2 b) { return a ^ b << ((sizeof(T) * 8) - 1); }
-template <typename T, typename... Bits>
-constexpr auto CombineBits(T head, Bits... bits) { return CombineBits(bits...) << ((sizeof(T) * 8) - 1) ^ head; }
