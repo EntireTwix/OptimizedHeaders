f466d6c2742a6209999b430cc80a4d2072cb2a0e
William Katz <willkatz.dev@gmail.com>
2021-04-04 21:57:53 -0700

    Force Inline macro

--- utils.hpp
+++ utils.hpp
@@ -10,3 +10,5 @@ template <typename T>
 struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &> {};
 template <typename T>
 using copy_fast_t = typename copy_fast<T>::type;
+
+#define INLINE __attribute__((always_inline)) inline
