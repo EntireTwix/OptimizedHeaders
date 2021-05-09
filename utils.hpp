88d2d5a717d45b1cd60a6da779d0244a61fd67f7
William Katz <willkatz.dev@gmail.com>
2021-05-08 19:45:37 -0700

    :sparkles: time func

--- utils.hpp
+++ utils.hpp
@@ -18,3 +18,14 @@ using copy_fast_cv_t = typename copy_fast_cv<T>::type;
 #define SET(v, i) v ^= (1 << i)
 #define ON(v, i) v |= (1 << i)
 #define OFF(v, i) v &= ~(1 << i)
+
+#include <ctime>
+#include <ratio>
+#include <chrono>
+#define time(f)                                                            \
+    {using namespace std::chrono;                                           \
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();   \
+    f;                                                                     \
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();   \
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1); \
+    std::cout << "It took me " << time_span.count() << " seconds.\n";}
