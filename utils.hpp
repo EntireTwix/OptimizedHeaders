9c607ac52f8af159c5fe45f93ebc2cf4a565d4f5
William Katz <qualitycontentquestionmark@gmail.com>
2021-06-19 14:06:15 -0700

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -23,6 +23,21 @@ using copy_fast_cv_t = typename copy_fast_cv<T>::type;
 #include <ratio>
 #include <chrono>
 
+#define time_func_a(f, a, x)                                                                  \
+    {                                                                                         \
+        using namespace std::chrono;                                                          \
+        uint32_t timer = 0;                                                                   \
+        for (int i = 0; i < x; ++i)                                                           \
+        {                                                                                     \
+            auto t1 = high_resolution_clock::now().time_since_epoch();                        \
+            f;                                                                                \
+            auto t2 = high_resolution_clock::now().time_since_epoch();                        \
+            a;                                                                                \
+            timer += std::chrono::duration_cast<std::chrono::nanoseconds>((t2 - t1)).count(); \
+        }                                                                                     \
+        std::cout << timer / x << '\n';                                                       \
+    }
+
 #define time_func(f, x)                                                                       \
     {                                                                                         \
         using namespace std::chrono;                                                          \
@@ -35,6 +50,12 @@ using copy_fast_cv_t = typename copy_fast_cv<T>::type;
             timer += std::chrono::duration_cast<std::chrono::nanoseconds>((t2 - t1)).count(); \
         }                                                                                     \
         std::cout << timer / x << '\n';                                                       \
+    }
+
+#define Op_a(v, name, x, a)   \
+    {                         \
+        std::cout << name;    \
+        time_func_a(v, a, x); \
     }
 
 #define Op(v, name, x)     \
