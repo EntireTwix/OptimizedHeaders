4b62aedb5b47087423ea554bbfd8481e717c3f17
William Katz <qualitycontentquestionmark@gmail.com>
2021-07-25 23:45:12 -0700

    :zap: C++17 support for copy_fast

--- utils.hpp
+++ utils.hpp
@@ -4,11 +4,12 @@
 //consteval for non-C++20
 template <auto ret>
 constexpr auto ConfirmConstexpr() { return ret; }
+
 //CopyFast metaprogramming type
 template <typename T>
 struct copy_fast : std::conditional<std::is_trivially_copyable_v<T>, T, const T &>{};
 template <typename T>
-struct copy_fast_cv : copy_fast<typename std::remove_cvref_t<T>>{};
+struct copy_fast_cv : copy_fast<std::remove_cv_t<std::remove_reference_t<T>>>{};
 template <typename T>
 using copy_fast_cv_t = typename copy_fast_cv<T>::type;
 
